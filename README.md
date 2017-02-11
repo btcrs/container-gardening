646a9be62142b80d89d1050716911f2ffb895e1c
Ben Carothers <btcrs@icloud.com>
2017-02-11 02:58:19 +0000

    Adds communication with REST service

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ Dockerized IOT components used to collect data on my hydroponic garden.
 
 ```
 docker build -t <tag> .
-docker run --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --privileged -ti btcrs/temperature
+docker run --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem --privileged -ti <tag>
+```
+To add env variables
+
+```
+docker run --device /dev/ttyAMA0:/dev/ttyAMA0 --device /dev/mem:/dev/mem -e [variables] --privileged -ti <tag>
+
 ```
--- temperature/1wire/simple_temperature.py
+++ temperature/1wire/simple_temperature.py
@@ -1,10 +1,20 @@
 import w1thermsensor 
 import schedule
+import requests
+import json
 import time
+import os
 
 def job():
     for sensor in w1thermsensor.W1ThermSensor.get_available_sensors():
-        print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+        temperature = sensor.get_temperature()
+        print("Sensor %s has temperature %.2f" % (sensor.id, temperature))
+        send_data(temperature)
+
+def send_data(temperature):
+    entry = json.dumps({'parameter': 'temperature', 'value': str(temperature)})
+    url = (os.environ['API'] + '/dev/datum')
+    requests.post(url, data=entry)
 
 schedule.every(1).minutes.do(job)
 
