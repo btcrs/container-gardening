f3969b53d77b8bd49231b025200ba3e78fdf4f15
Ben Carothers <btcrs@icloud.com>
2017-03-27 15:45:26 -0400

    Adds reporting to all zero containers

--- pressure/pressure.py
+++ pressure/pressure.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 import time
+import schedule
+
+import sys
+sys.path.insert(0, '../reporter')
+
+from reporter import reporter
 from envirophat import weather, leds
 
-try:
-    while True:
-        pressure = weather.pressure()
-        print("{}".format(pressure))
-        time.sleep(0.1)
-except:
-    pass
+reporter = reporter()
+
+def get_data():
+    pressure = weather.pressure()
+    reporter.send_data("temperature", temperature)
+
+    schedule.every(1).minutes.do(get_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- temperature/temperature.py
+++ temperature/temperature.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 import time
+import schedule
+
+import sys
+sys.path.insert(0, '../reporter')
+
+from reporter import reporter
 from envirophat import weather, leds
 
+reporter = reporter()
 
-try:
-    while True:
-        temperature = weather.temperature()
-        print("{} degrees celcius".format(temperature))
-        time.sleep(0.1)
+def get_data():
+    temperature = weather.temperature()
+    reporter.send_data("temperature", temperature)
+
+    schedule.every(1).minutes.do(get_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
